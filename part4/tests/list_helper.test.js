884472ca40385979667456a20b72ab58e0b9e453
Suvi Vasama <suvi.vasama@gmail.com>
2024-01-16 13:50:36 +0200

    suggested solution to exercises 4.3-4.4

---
+++ part4/tests/list_helper.test.js
@@ -0,0 +1,62 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  const emptyBlogList = []
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },{
+      _id: '65a54d1a3eb83c19e9df7866',
+      title: '8 Blog Topic Generators for Blog Post Idea Inspiration',
+      author: 'Megan Marrs',
+      url: 'https://www.wordstream.com/blog/ws/2015/02/12/blog-topic-generators',
+      likes: 38,
+      __v: 0
+    },{
+      _id: '65a54d1a3eb83c19e9df7866',
+      title: 'Blogin kirjoittaminen: Askel askeleelta kohti menestyvää blogia',
+      author: 'Mikaela Laukkanen',
+      url: 'https://www.wordstream.com/blog/ws/2015/02/12/blog-topic-https://bukra.fi/blogin-kirjoittaminen/',
+      likes: 2,
+      __v: 0
+    }
+
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(emptyBlogList)
+    expect(result).toBe(0)
+  })
+  
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    expect(result).toBe(45)
+  })
+})
